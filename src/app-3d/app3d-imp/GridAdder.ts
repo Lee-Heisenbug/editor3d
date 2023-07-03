9bc08d9ae8fb18cd2f437503f47b26811922bf84
Lee-Heisenbug <1070068427@qq.com>
2023-07-03 22:40:05 +0800

    忽略网格和包围盒的射线检测

--- src/app-3d/app3d-imp/GridAdder.ts
+++ src/app-3d/app3d-imp/GridAdder.ts
@@ -1,9 +1,16 @@
 import { GridHelper } from 'three'
 import type { Infra, Initiator } from '../App3D'
 
+class IntersectionIgnoreGrid extends GridHelper {
+  raycast(): void {
+    this._doNothingToIgnoreIntersection()
+  }
+  _doNothingToIgnoreIntersection() {}
+}
+
 export class GridAdder implements Initiator {
   initiate({ scene }: Infra): void {
-    const grid = new GridHelper(30, 30)
+    const grid = new IntersectionIgnoreGrid(30, 30)
     scene.add(grid)
   }
 }
--- src/app-3d/app3d-imp/ObjectScaleInitiator.ts
+++ src/app-3d/app3d-imp/ObjectScaleInitiator.ts
@@ -4,11 +4,18 @@ import { ObjectScale } from '../ObjectScale'
 import { MouseDOMImp } from '../object-scale-imp/MouseDOMImp'
 import { SceneThreeImp } from '../object-scale-imp/SceneThreeImp'
 
+class IntersectionIgnoreBox3Helper extends Box3Helper {
+  raycast(): void {
+    this._doNothingToIgnoreIntersection()
+  }
+  _doNothingToIgnoreIntersection() {}
+}
+
 export class ObjectScaleInitiator implements Initiator {
   initiate(infra: Infra): void {
     const raycaster = new Raycaster()
     const box = new Box3()
-    const boxHelper = new Box3Helper(box)
+    const boxHelper = new IntersectionIgnoreBox3Helper(box)
     const objectScale = new ObjectScale(
       new MouseDOMImp(infra.renderer.domElement),
       new SceneThreeImp(
