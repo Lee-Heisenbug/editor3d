bba4d99dbce4887681e3e0ddcda31c45626ea07c
Lee-Heisenbug <1070068427@qq.com>
2023-07-13 21:35:44 +0800

    当鼠标点击和抬起的位置一致时才出发点击事件

--- src/app-3d/object-scale-imp/MouseDOMImp.ts
+++ src/app-3d/object-scale-imp/MouseDOMImp.ts
@@ -1,13 +1,29 @@
-import type { Mouse, MouseEventCallback } from '../ObjectScale'
+import type { Mouse, MouseEventCallback, MousePosition } from '../ObjectScale'
 
 export class MouseDOMImp implements Mouse {
   private _dom: HTMLElement
+  private _clickCallback: MouseEventCallback = () => {}
+  private _screenPosOnMouseDown: MousePosition = [0, 0]
   constructor(dom: HTMLElement) {
     this._dom = dom
+    this._initiate()
   }
-  onClick(callback: MouseEventCallback): void {
-    this._dom.addEventListener('click', (e) => {
-      callback([e.offsetX, e.offsetY])
+
+  _initiate() {
+    this._dom.addEventListener('mousedown', (e) => {
+      this._screenPosOnMouseDown = [e.offsetX, e.offsetY]
     })
+    this._dom.addEventListener('mouseup', (e) => {
+      const screenPosOnMouseUp: MousePosition = [e.offsetX, e.offsetY]
+      if (screenPosOnMouseUp[0] === this._screenPosOnMouseDown[0]) {
+        if (screenPosOnMouseUp[1] === this._screenPosOnMouseDown[1]) {
+          this._clickCallback(screenPosOnMouseUp)
+        }
+      }
+    })
+  }
+
+  onClick(callback: MouseEventCallback): void {
+    this._clickCallback = callback
   }
 }
