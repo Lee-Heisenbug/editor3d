1a63aab786b5307f59dfbfb9ff97604b45ac64d0
Lee-Heisenbug <1070068427@qq.com>
2023-07-26 00:19:19 +0800

    新增vuetify

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,14 @@ onMounted(() => {
     app3d.initiate()
   }
 })
+
+let text = ref(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!`)
 </script>
 
 <template>
   <div class="editor3d">
     <canvas ref="app3dCanvas" class="app-3d"></canvas>
+    <v-card title="Card title" :text="text"></v-card>
   </div>
 </template>
 
