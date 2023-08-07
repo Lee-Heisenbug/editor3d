1068864196e3da71d50f252bdef786d50c42a3b6
Lee-Heisenbug <1070068427@qq.com>
2023-08-08 00:13:03 +0800

    取消绑定opened

--- src/components/SceneHierarchy.vue
+++ src/components/SceneHierarchy.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-list class="hierarchy" :opened="[]">
+  <v-list class="hierarchy">
     <HierarchyNode :key="nodeData.id" v-for="nodeData in data" :data="nodeData"></HierarchyNode>
   </v-list>
 </template>
